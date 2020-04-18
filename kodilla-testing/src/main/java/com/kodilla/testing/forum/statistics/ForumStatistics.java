6209d48bcaf3fd0b69df216346beb63d26ae231a
Piotr <piotr.rybak1@gazeta.pl>
2020-04-18 14:14:20 +0200

    zad.6.5

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/ForumStatistics.java
@@ -0,0 +1,33 @@
+package com.kodilla.testing.forum.statistics;
+
+
+import java.util.ArrayList;
+
+public class ForumStatistics {
+    Integer userNumber;
+    Double postsNumber;
+    Double commentsNumber;
+    Double averagePosts;
+    Double averageComments;
+    Double averageCommentsPosts;
+
+    public void calculateAdvStatistics(Statistics statistics) {
+
+        ArrayList<String> userList = new ArrayList<>();
+        for (String name : statistics.usersNames()) {
+            userList.add(name);
+        }
+        userNumber = userList.size();
+        postsNumber = (double) statistics.postsCount();
+        commentsNumber = (double) statistics.commentsCount();
+        averagePosts = (postsNumber/userNumber);
+        averageComments = (commentsNumber /userNumber);
+        averageCommentsPosts = (commentsNumber / postsNumber);
+
+    }
+
+    public void showStatistics() {
+       System.out.println( " Numbers of users = " +userNumber +" Numbers of posts = "+  postsNumber + " Numbers of comments = "+  commentsNumber +" Average of posts = " + averagePosts +" Average of comments = " +  averageComments + " Average comments/posts = "+ averageCommentsPosts);
+    }
+
+}
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/ForumStatisticsTest.java
@@ -0,0 +1,81 @@
+package com.kodilla.testing.forum.statistics;
+
+import org.junit.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ForumStatisticsTest {private static int testCounter = 0;
+
+    @BeforeClass
+    public static void beforeAllTests() {
+        System.out.println("This is the beginning of tests.");
+    }
+
+    @AfterClass
+    public static void afterAllTests() {
+        System.out.println("All tests are finished.");
+    }
+
+    @Before
+    public void beforeEveryTest() {
+        testCounter++;
+        System.out.println("Preparing to execute test #" + testCounter);
+    }
+
+    @Test
+    public void testcalculateAdvStatistics() {
+
+        //Given
+        ForumStatistics forumStatistics = new ForumStatistics();
+
+        Statistics statistics = mock(Statistics.class);
+        forumStatistics.calculateAdvStatistics( statistics);
+        Random theGenerator = new Random();
+        List<String> result = new ArrayList<>();
+        for (int n=0; n<100; n++){
+
+            result.add(String.join("", Collections.nCopies(theGenerator.nextInt(50) + 1, "a")));
+        }
+
+        when(statistics.usersNames()).thenReturn((ArrayList) result);
+        Integer posts = 1000;
+        when(statistics.postsCount()).thenReturn((Integer) posts);
+        Integer comments = 10000;
+        when(statistics.commentsCount()).thenReturn((Integer) comments);
+
+
+        //When
+        int quantityOfUsers = result.size();
+        int postsStat = posts;
+        int commentsStat = comments;
+        //Then
+        assertEquals(100, quantityOfUsers);
+        assertEquals(1000,postsStat);
+        assertEquals(10000,commentsStat);
+
+    }
+
+    @Test
+    public void testshowStatistics(){
+
+        ForumStatistics forumStatistics = new ForumStatistics();
+        Integer userNumber = 100;
+        Double postsNumber = 1000.00;
+        Double commentsNumber= 10000.00;
+        Double averagePosts = 10.00;
+        Double averageComments= 10.00;
+        Double averageCommentsPosts = 10.00;
+
+         forumStatistics.showStatistics();
+
+
+    }
+
+}
