5c71b6b2649a97df6a6611a6cd67ef74410f593a
Piotr <piotr.rybak1@gazeta.pl>
2020-05-10 00:52:35 +0200

    Revert "Revert "zadanie 8.3 prawidłowe pliki""
    This reverts commit bb4b2c63

--- kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -1,17 +1,4 @@
 package com.kodilla.exception.test;
 
 public class ExceptionHandling {
-    public static void main(String[] args) {
-        SecondChallenge secondChallenge = new SecondChallenge();
-        try {
-             String secondChallengeResult = secondChallenge.probablyIWillThrowException(1.0, 2.0);
-             System.out.println(secondChallengeResult);
-
-        } catch (Exception e) {
-            System.out.println("There is a problem check it out: " + e);
-        } finally {
-            System.out.println("Remember your numbers must be different from: (x >= 2 || x < 1 || y == 1.5)");
-
-        }
-    }
 }
