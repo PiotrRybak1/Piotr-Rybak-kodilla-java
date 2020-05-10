4bc7f14de3a029a9541827471f2a3ceab561bdbe
Piotr <piotr.rybak1@gazeta.pl>
2020-05-10 03:54:21 +0200

    zadanie 8.3 prawidłowe pliki

--- kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -1,4 +1,19 @@
 package com.kodilla.exception.test;
 
 public class ExceptionHandling {
+    public static void main(String[] args){
+        SecondChallenge secondChallenge = new SecondChallenge();
+
+        try{
+            String probablyIWillThrowExceptionResult = secondChallenge.probablyIWillThrowException(5.0,2.5);
+            System.out.println(probablyIWillThrowExceptionResult);
+
+        } catch (Exception e) {
+            System.out.println("A problem has occurred, look:  " + e );
+
+        }finally {
+         System.out.println(" Remember the range of your numbers must be different than: x >= 2 || x < 1 || y == 1.5");
+
+        }
+    }
 }
