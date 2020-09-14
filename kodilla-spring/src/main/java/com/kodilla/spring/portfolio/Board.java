a6135405df59fc1851b57f2c6a7987fafe012158
Piotr <piotr.rybak1@gazeta.pl>
2020-09-14 21:15:27 +0200

    poprawki 14.3 board.java

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/Board.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/Board.java
@@ -1,9 +1,9 @@
 package com.kodilla.spring.portfolio;
 
 public class Board {
-    TaskList toDoList;
-    TaskList inProgressList;
-    TaskList doneList;
+   private final TaskList toDoList;
+   private  final TaskList inProgressList;
+   private final TaskList doneList;
 
     public Board(TaskList toDoList, TaskList inProgressList, TaskList doneList) {
         this.toDoList = toDoList;
