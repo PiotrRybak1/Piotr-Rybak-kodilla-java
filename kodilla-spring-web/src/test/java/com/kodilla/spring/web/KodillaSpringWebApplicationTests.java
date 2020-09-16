cc364a804c807d194a4f93a4b19d26f5c4de7cbf
Piotr <piotr.rybak1@gazeta.pl>
2020-09-17 00:22:38 +0200

    zadanie 14.4

---
+++ kodilla-spring-web/src/test/java/com/kodilla/spring/web/KodillaSpringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.spring.web;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaSpringWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
